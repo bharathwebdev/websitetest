8f4d61036ec9e7c322eec8bb2a6705ad6c34b990
bharath <bharath18117886@gmail.com>
2022-10-15 14:19:06 +0530

    new

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-
-import Form from "./component/Form";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
